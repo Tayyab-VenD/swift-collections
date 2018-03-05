04daa4f87f1852f19f6d3bca6cebe92c8e657d17
Muhammad Tayyab Akram <dear_tayyab@yahoo.com>
2018-03-05 14:58:49 +0500

    Initial Commit

---
+++ Collections/Collections.h
@@ -0,0 +1,19 @@
+//
+//  Collections.h
+//  Collections
+//
+//  Created by Muhammad Tayyab Akram on 05/03/2018.
+//  Copyright © 2018 Muhammad Tayyab Akram. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Collections.
+FOUNDATION_EXPORT double CollectionsVersionNumber;
+
+//! Project version string for Collections.
+FOUNDATION_EXPORT const unsigned char CollectionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Collections/PublicHeader.h>
+
+
