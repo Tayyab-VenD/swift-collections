6b0ec6c68fcb978e99a3b404926ace313daa15f8
Muhammad Tayyab Akram <dear_tayyab@yahoo.com>
2018-03-13 13:12:53 +0500

    Exposed nodes collection in linked collection...

--- Collections/LinkedCollection.swift
+++ Collections/LinkedCollection.swift
@@ -10,14 +10,58 @@ import Foundation
 
 public protocol LinkedCollection : Collection {
     associatedtype Node
+    associatedtype Nodes : Collection = DefaultNodes<Self>
+        where Nodes.Element == Node,
+              Nodes.SubSequence == Nodes
 
     func node(at index: Index) -> Node
+    var nodes: Nodes { get }
 }
 
-public protocol LinkedMutableCollection : LinkedCollection, MutableCollection {
+extension LinkedCollection {
+    public var nodes: DefaultNodes<Self> {
+        return DefaultNodes(elements: self, startIndex: startIndex, endIndex: endIndex)
+    }
 }
 
-public protocol LinkedRangeReplaceableCollection : LinkedMutableCollection, RangeReplaceableCollection {
-    mutating func append(node: Node)
-    mutating func insert(node: Node, at i: Index)
+public struct DefaultNodes<Elements: LinkedCollection> {
+    private var elements: Elements
+    public private(set) var startIndex: Index
+    public private(set) var endIndex: Index
+
+    init(elements: Elements, startIndex: Index, endIndex: Index) {
+        self.elements = elements
+        self.startIndex = startIndex
+        self.endIndex = endIndex
+    }
+}
+
+extension DefaultNodes : Collection {
+    public typealias Index = Elements.Index
+    public typealias Element = Elements.Node
+    public typealias Indices = Elements.Indices
+    public typealias SubSequence = DefaultNodes<Elements>
+
+    public subscript(i: Index) -> Element {
+        return elements.node(at: i)
+    }
+
+    public subscript(bounds: Range<Index>) -> DefaultNodes<Elements> {
+        return DefaultNodes(
+            elements: elements,
+            startIndex: bounds.lowerBound,
+            endIndex: bounds.upperBound)
+    }
+
+    public func index(after i: Index) -> Index {
+        return elements.index(after: i)
+    }
+
+    public func formIndex(after i: inout Index) {
+        elements.formIndex(after: &i)
+    }
+
+    public var indices: Indices {
+        return elements.indices
+    }
 }
